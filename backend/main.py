15f13ccc86da7d4b14fb6171eb6d96513dcca960
Olegogonich <olegogonich240708@gmail.com>
2025-10-25 12:02:57 +0300

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -19,6 +19,8 @@ from src.models import UserRegistration, AuthorizationHeader
 app = FastAPI()
 
 origins = [
+    "http://127.0.0.1:3000",
+    "http://127.0.0.1"
     "http://localhost",
     "http://localhost:8000",
 ]
